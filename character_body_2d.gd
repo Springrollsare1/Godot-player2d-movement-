bc30e47b25ad4f95c48adf9d97b98afcad8d7b3d
AAstromakesgames <onedaythatcowwillrise@gmail.com>
2024-12-23 13:48:17 -0500

    Update character_body_2d.gd

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -4,10 +4,6 @@ extends CharacterBody2D
 var speed : int
 var screen_size : Vector2
 
-func _ready():
-	screen_size = get_viewport_rect().size
-	position = screen_size / 2
-	speed = 150
 func get_input(): 
 	#keyboard input
 	var input_dir = Input.get_vector("left", "right","up", "down") 
